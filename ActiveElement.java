6ba04acee698dba391a9d19f513b7b48a28938d1
Hamza <stitihamza12@gmail.com>
2023-04-16 19:43:06 +0200

    S

--- ActiveElement.java
+++ ActiveElement.java
@@ -5,7 +5,7 @@ public abstract class ActiveElement {
     private int id ;// the id is for  the pump I did not use it yet  //
     private WaterTank water_tank ;
 
-    public void TransferWater(){}
+//    public void TransferWater(){}
 
 
 }
